0d00aa6a1c211df260ae1fd25029c6be3ae39ad4
Naveen Valecha <naveen.valecha@morpht.com>
2024-01-31 05:59:08 +0000

    convivial-demo-36062: Use docker image which has memory_limit ovrriden.
    Mirrored from (c-36062).

--- js/gallery.js
+++ js/gallery.js
@@ -4,12 +4,8 @@
       const collection = document.getElementsByClassName('gallery');
       for (let item of collection) {
         let showControls = !item.classList.contains('gallery--hide-controls');
-        const classes = item.className;
-        // Replace whitespace for a dot.
-        const selector = classes.replace(/\s/g, '.')
-
         GLightbox({
-          selector: '.' + selector + ' a.img',
+          selector: '#' + item.id + ' a.img',
           touchNavigation: showControls,
           loop: showControls,
         });
