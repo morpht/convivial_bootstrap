9c862750144258e7288b9680b0b8e2346043e12c
Jan Pára <honza@morpht.com>
2024-10-09 09:26:42 +0000

    convivial-demo-38407: Add once to animation script.
    Mirrored from (c-38407).

--- components/animation/animation.js
+++ components/animation/animation.js
@@ -1,4 +1,4 @@
-(function ($, Drupal, drupalSettings) {
+(function ($, Drupal, drupalSettings, once) {
   Drupal.behaviors.convivialAnimation = {
     attach: function (context) {
       if (typeof drupalSettings.animation !== 'undefined') {
@@ -9,16 +9,17 @@
             delay = animations[paragraphId].delay,
             repeat = animations[paragraphId].repeat;
 
-          // init controller
-          let controller = new ScrollMagic.Controller();
+          // Ensure this behavior runs only once per element
+          $(once('convivial-animation', 'div.paragraph--type--animation.paragraph--id-' + paragraphId, context)).each(function () {
+            // init controller
+            let controller = new ScrollMagic.Controller();
 
-          // TweenMax can tween any property of any object. We use this object
-          // to cycle through the array
-          let obj = { curImg: 0 };
+            // TweenMax can tween any property of any object. We use this object
+            // to cycle through the array
+            let obj = { curImg: 0 };
 
-          // create tween
-          let tween = TweenMax.to(obj, 0.5,
-            {
+            // create tween
+            let tween = TweenMax.to(obj, 0.5, {
               // animate property curImg to number of images
               curImg: imagePaths.length - 1,
               // only integers so it can be used as an array index
@@ -30,20 +31,21 @@
               // show every image the same amount of time
               ease: Linear.easeNone,
               onUpdate: function () {
-                $('img.field--paragraph-' + paragraphId).attr('src', imagePaths[obj.curImg]); // set the image source
-              },
-            },
-          );
+                // Update the image source
+                $('img.field--paragraph-' + paragraphId, context).attr('src', imagePaths[obj.curImg]);
+              }
+            });
 
-          // build scene
-          new ScrollMagic.Scene({
-            triggerElement: 'div.paragraph--type--animation.paragraph--id-' + paragraphId,
-            duration: delay,
-          })
-            .setTween(tween)
-            .addTo(controller);
+            // build scene
+            new ScrollMagic.Scene({
+              triggerElement: this,
+              duration: delay
+            })
+              .setTween(tween)
+              .addTo(controller);
+          });
         }
       }
-    },
+    }
   };
-})(jQuery, Drupal, drupalSettings);
+})(jQuery, Drupal, drupalSettings, once);
