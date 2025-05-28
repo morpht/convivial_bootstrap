8774818cdbe944645b16546b2f4c1526a4630264
Jan Pára <honza@morpht.com>
2025-05-28 11:21:07 +0000

    convivial-demo-42240: Animate fast fact when scroll into view.
    Mirrored from (c-42240).

--- components/fastfacts/fastfacts.js
+++ components/fastfacts/fastfacts.js
@@ -3,28 +3,59 @@
  * JS for Fast Facts counting effect.
  */
 
-(function ($, Drupal) {
+(function (Drupal) {
   Drupal.behaviors.fastfacts = {
     attach: function (context, settings) {
-      function count($this, number, context){
-        $({ countNum: $this.html(), context:context }).animate({ countNum: number }, {
-          duration: 3000,
-          easing: 'linear',
-          step: function () {
-            $this.html(Math.floor(this.countNum).toLocaleString());
-          },
-          complete: function () {
-            $this.html(this.countNum.toLocaleString());
-          }
-        });
+      const fastFacts = context.querySelectorAll('.fastfact__value')
+      let fastFactsItem = []
+
+      if (!fastFacts) {
+        return
       }
 
-      $(".fastfact__value", context).each(function () {
-        var number = parseInt($(this).html(), 10);
-        $(this).html('0');
-        count($(this), number, context);
-      });
-    }
-  };
+      // Construct fast fact list item
+      fastFacts.forEach(function (item, index) {
+        const endNumber = parseInt(item.innerHTML, 10)
+        fastFactsItem.push(item)
+        fastFactsItem[index]['trigger'] = false
+        fastFactsItem[index]['startNumber'] = 0
+        fastFactsItem[index]['endNumber'] = endNumber
+        item.innerHTML = '0'
+      })
 
-})(jQuery, Drupal);
+      context.addEventListener('scroll', function () {
+        fastFactsItem.forEach(function (item) {
+          if (isScrolledIntoView(item) && item['trigger'] !== true) {
+            item['trigger'] = true
+            animateValue(item, item['startNumber'], item['endNumber'], 3000)
+          }
+        })
+      })
+
+      // Utility
+      function isScrolledIntoView (element) {
+        const rect = element.getBoundingClientRect()
+        const elemTop = rect.top
+        const elemBottom = rect.bottom
+        return (elemTop >= 0) && (elemBottom <= window.innerHeight)
+      }
+
+      function animateValue (item, start, end, duration) {
+        if (start === end) {
+          return
+        }
+        const range = end - start
+        let current = start
+        const increment = end > start ? 1 : -1
+        const stepTime = Math.abs(Math.floor(duration / range))
+        const timer = setInterval(function () {
+          current += increment
+          item.innerHTML = current
+          if (current === end) {
+            clearInterval(timer)
+          }
+        }, stepTime)
+      }
+    },
+  }
+})(Drupal)
